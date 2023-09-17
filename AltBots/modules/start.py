90017971539cce44af9d545a4420b9ba27a4b687
Divyanshu Rana <141987294+Ranavanshi@users.noreply.github.com>
2023-09-17 21:39:48 +0530

    Update start.py

--- AltBots/modules/start.py
+++ AltBots/modules/start.py
@@ -8,7 +8,7 @@ START_BUTTON = [
         Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
     ],
     [
-        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/Ranavanshi_divy"),
+        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/webnet_bot_support"),
         Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/chatventure_club")
     ],
     [
